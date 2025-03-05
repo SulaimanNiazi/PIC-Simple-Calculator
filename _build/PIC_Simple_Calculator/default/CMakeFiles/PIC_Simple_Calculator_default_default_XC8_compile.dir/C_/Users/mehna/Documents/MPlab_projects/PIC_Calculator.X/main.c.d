9d5a9ec8a1e50d979f104dfa9c4de34a0dc27f08
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-03-05 10:27:47 +0500

    Add necessary MPlab project files

---
+++ _build/PIC_Simple_Calculator/default/CMakeFiles/PIC_Simple_Calculator_default_default_XC8_compile.dir/C_/Users/mehna/Documents/MPlab_projects/PIC_Calculator.X/main.c.d
@@ -0,0 +1,2 @@
+CMakeFiles\PIC_Simple_Calculator_default_default_XC8_compile.dir\C_\Users\mehna\Documents\MPlab_projects\PIC_Calculator.X\main.c.p1: \
+  C:/Users/mehna/Documents/MPlab\ projects/PIC\ Calculator.X/main.c
---
+++ _build/PIC_Simple_Calculator/default/doscan.d
@@ -0,0 +1,1 @@
+doscan.p1: c:/PROGRA~1/MICROC~1/xc8/v3.00/pic/sources/c99/common/doscan.c
